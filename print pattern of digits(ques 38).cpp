987468d21f31e0a4dc0ea72f4c4f8971cb9c8129
Smrity chaudhary <smrity.mca17.du@gmail.com>
2017-12-22 13:21:15 +0530

    correction

--- print pattern of digits(ques 38).cpp
+++ print pattern of digits(ques 38).cpp
@@ -11,14 +11,10 @@ int main(){
   cout << "Enter an integer(0 to 32,767): ";
   cin >> input;
   int last = static_cast<int>(input) % 10;
-  int med = (input - last) / 10;
-  int last2 = med % 10;
-  med = (med - last2) / 10;
-  int last3 = med % 10;
-  med = (med - last3) / 10;
-  int last4 = med % 10;
-  med = (med - last4) / 10;
-  int last5 = med % 10;
+  int last2 = (static_cast<int>(input) / 10) % 10;
+  int last3 = (static_cast<int>(input) / 100 ) % 10;
+  int last4 = (static_cast<int>(input) / 1000) % 10;
+  int last5 = (static_cast<int>(input) / 10000) % 10;
 
   cout << last5 << "   " << last4 << "   " << last3 << "   " << last2 << "   "<< last <<'\n';
   cout << last4 << "   " << last3 << "   " << last2 << "   "<< last <<'\n';
