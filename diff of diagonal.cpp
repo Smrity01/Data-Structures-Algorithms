b1b8ee8ab21638fc221dac1cad100dc233b93bf9
Smrity chaudhary <smrity.mca17.du@gmail.com>
2018-01-04 23:06:48 +0530

    Update diff of diagonal.cpp

--- diff of diagonal.cpp
+++ diff of diagonal.cpp
@@ -22,7 +22,7 @@ int main(){
     int secondary_diag = 0;
     for(int i = 0; i < length; i++){
             for(int j = 0;j < length;j++){
-                    if (i==j && (i + j) == 2){
+                    if (i==j && (i + j) == length-1){
                         primary_diag = primary_diag + matrix[i][j];
                         secondary_diag = secondary_diag + matrix[i][j];
                     }
