796115ea521d48fe5456077b9bede7b1f65339ab
Smrity chaudhary <smrity.mca17.du@gmail.com>
2018-01-05 22:31:21 +0530

    count set bits using logical and

--- count set bits.cpp
+++ count set bits.cpp
@@ -1,26 +1,43 @@
 /*
 Calculate the set bits in a number
    written by: Smrity chaudhary
-   Date : 2-1-18
+   Date : 5-01-2018
 */
 #include<iostream>
 using namespace std;
 
+int power(int);
 int binary(int);
+int calculate(int);
 int main(){
-    int num;
+    int num =4&3;
     cout << "Enter a number: ";
     cin >> num;
-    cout << "Number of set bits: " << binary(num);
+    cout << "Number of set bits: "<< binary(num);
 }
 int binary(int num){
-    int counter=0;
-    while(num != 0){
-        if(num % 2 == 1){
-            counter++;
-        }
-        num = num / 2;
+    if (num != 0 && num == power(num)){
+        return 1;
     }
-    return counter;
-}
+    else{
+    return (calculate(num));}
 
+}
+int power(int num){
+    return (num & ~(num-1));
+}
+int calculate(int num){
+    int total = 0;
+    int prev = num;
+    int temp = num;
+    while(temp!=0){
+            temp = temp & num-1;
+            num = num - 1;
+            if(temp != prev){
+                total++;
+            }
+            prev = temp;
+    }
+
+    return total;
+}
