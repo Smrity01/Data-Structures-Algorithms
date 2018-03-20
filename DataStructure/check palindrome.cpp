fdbff794cde0e77a58c45850aaf65436080833d4
Smrity chaudhary <smrity.mca17.du@gmail.com>
2018-03-20 11:21:54 +0530

    documentation

--- DataStructure/check palindrome.cpp
+++ DataStructure/check palindrome.cpp
@@ -1,3 +1,8 @@
+/*
+Check the palindrome using STL Stack and Queue
+Written By : Smrity Chaudhary
+  Dated    : 14/03/2018 
+*/
 #include<iostream>
 #include<conio.h>
 #include<string>
@@ -8,6 +13,13 @@ using namespace std;
 
 void check_palindrome(queue<char>,stack<char>);
 int main(){
+  /*
+  Objective       : Create Object of stack and Queue class and take input from user
+  Input Parameter : None
+  Output Parameter: None
+  Description     : Main function
+  Approach        : Take string from user and push one by one character of string into stack and Queue using for loop 
+  */
   queue <char> object;
   stack <char> s;
   string word;
@@ -24,18 +36,21 @@ int main(){
   return 0;
 }
 void check_palindrome(queue<char> q,stack<char> s) {
-	while(!s.empty()){
-		if(q.front()!=s.top()){
-			cout << "\nIts NOT a PALINDROME..";
-			return;
-		}
-		s.pop();
-		q.pop();
+   /*
+  Objective       : CHeck whether the string is a palindrome or not
+  Input Parameter : object of Queue and Stack class  
+  Output Parameter: Print the status of string
+  Description     : Void function
+  Approach        : Pop one by one character of string from (last character) Stack and (first character) Queue using while loop and compare them 
+  */
+  while(!s.empty()){
+	if(q.front()!=s.top()){
+		cout << "\nIts NOT a PALINDROME..";
+		return;
 	}
-	cout << "Its a PALINDROME..";
-	return;
-	
-	
+	s.pop();
+	q.pop();
+ }
+ cout << "Its a PALINDROME..";
+return;	
 }
-
-
