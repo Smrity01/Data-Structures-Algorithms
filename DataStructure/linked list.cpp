4794c9d2b60ffc56d525a20c4b4a90483e5eecab
Smrity chaudhary <smrity.mca17.du@gmail.com>
2018-03-21 22:10:59 +0530

    Update linked list.cpp

--- DataStructure/linked list.cpp
+++ DataStructure/linked list.cpp
@@ -14,15 +14,25 @@ class linkedlist{
 		node<T>* head;
 		linkedlist();
 		void insertion(T);
+		bool isempty();
 		void end_deletion();
 		void front_deletion();
 		void element_deletion(T);
-		//void front_insertion(T);
+		void front_insertion(T);
 		void display();
 };
 template <class T>
 linkedlist<T>::linkedlist(){
 	head = NULL;
+}
+template <class T>
+bool linkedlist<T>::isempty(){
+	if(head == NULL){
+		return true;
+	}
+	else{
+		return false;
+	}
 }
 template <class T>
 void linkedlist<T>::insertion(T element){
@@ -47,7 +57,6 @@ void linkedlist<T>::display(){
 		cout << "\nList is empty...!";
 		return;
 	}
-	cout << "\n LL\n";
 	while(temp != NULL ) {
 		cout << temp->data <<",";
 		temp = temp->next;
@@ -112,17 +121,55 @@ void linkedlist<T>::element_deletion(T element){
 	delete curr;
 	
 }
-//template <class T>
-//void linkedlist<T>::
+template <class T>
+void linkedlist<T>::front_insertion(T element){
+	node<T>* newnode = new node<T>;
+	newnode->next = NULL;
+	newnode->data = element;
+	if(head == NULL){
+		head = newnode;
+		return;
+	}
+	else{
+		newnode->next = head;
+		head = newnode;
+		return;
+	}
+}
 int main(){
 	linkedlist<int> object;
-	object.insertion(1);
-	object.insertion(15);
-	object.insertion(16);
-	object.insertion(31);
-	object.display();
-	object.element_deletion(31);
-	object.display();
+	int choice;
+	while(1){
+		cout <<"\n\n\n***********MENU*********\n1.Insertion at Back \n2.Insertion At front \n3.Delete Front \n4.Delete Back \n5.Delete element \n6.DISPLAY \n7.EXIT";
+		cout << "\nEnter your choice: ";
+	    cin >> choice;
+	
+	   switch(choice){
+		    case 1: int element;
+		            cout << "Enter an element: ";
+		            cin >> element;
+				    object.insertion(element);
+				    break;
+			case 2: cout << "Enter an element: ";
+		            cin >> element;
+				    object.front_insertion(element);
+				    break;
+		    case 3: object.front_deletion();
+				    break;
+		    case 4: object.end_deletion();
+		            break;
+		            
+		    case 5: cout << "Enter an element: ";
+		            cin >> element;
+				    object.element_deletion(element);
+		            break;
+			case 6: cout <<"\nLinked list is: " ;
+		            object.display();
+		            break;
+		    case 7: exit(0);
+		    default: cout << "\nWrong choice..."; 
+		}
+	}
 	getch();
 	return 0;
 }
