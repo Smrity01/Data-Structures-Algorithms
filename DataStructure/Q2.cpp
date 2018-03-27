b6b25f75318fecc88fee6da63ca03271b6e19309
Smrity chaudhary <smrity.mca17.du@gmail.com>
2018-03-27 23:47:18 +0530

    documentation

--- DataStructure/Q2.cpp
+++ DataStructure/Q2.cpp
@@ -1,3 +1,8 @@
+/*
+Reverse in Pair of linked list
+Written by : Smrity Chaudhary
+     Dated : 20-03-2018
+*/
 #include<iostream>
 #include<conio.h>
 #include<math.h>
@@ -8,12 +13,19 @@ using namespace std;
 bool check_consecutive(int,int);
 void all_consecutive(stack<int>);
 int main(){
+	/*
+	Objective       : Main function of program
+	Input parameter : None
+	Output Values   : None
+	Description     : Calling the required function of class
+	Approach        : Take input form user and according to the choice the class function is called 
+	*/
   stack <int> s;
   int count;
   cout <<" How many elements do you want to enter: ";
   cin >> count;
   int element;
-  for (int i;i<count;i++) {
+  for (int i=0;i<count;i++) {
     cout << "\nEnter element: ";
     cin >> element;
     s.push(element);
@@ -24,6 +36,13 @@ int main(){
 }
 
 bool check_consecutive(int num1,int num2){
+	/*
+	Objective       : To check for consecutive pairs
+	Input parameter : Two numbers which need to be check
+	Output Values   : None
+	Description     : Boolean function definition
+	Approach        : Compare both the numbers
+	*/
 	if(fabs(num1-num2)==1){
 		return true;
 	}
@@ -33,7 +52,14 @@ bool check_consecutive(int num1,int num2){
 }
 
 void all_consecutive(stack<int> s){
-    queue <int> q;
+	/*
+	Objective       : To check for All consecutive pairs in stack
+	Input parameter : Object of stack
+	Output Values   : None
+	Description     : Void function definition
+	Approach        : By pushing top element of stack into Queue and compairing it with immediate top of the stack
+	*/    
+	queue <int> q;
 	int size_stack = s.size();
 	if(s.empty()){
 		cout<<"\nstack is empty...";
