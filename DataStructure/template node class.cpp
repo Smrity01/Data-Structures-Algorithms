4c1c122a8d73f5e58e65db934473f3cb70644e40
Smrity chaudhary <smrity.mca17.du@gmail.com>
2018-06-13 23:11:23 +0530

    documentation

--- DataStructure/template node class.cpp
+++ DataStructure/template node class.cpp
@@ -1,3 +1,8 @@
+/*
+Implementation of linked list(using node class) - insertions and deletions
+Written by : Smrity Chaudhary
+    Dated  : 23/03/2018
+*/
 #include<iostream>
 #include<conio.h>
 #include<cstring>
@@ -6,12 +11,26 @@ using namespace std;
 template <class T> class linkedlist;
 template <class T>
 class node{
+	/*
+	Objective       : Creating a class Node 
+	Input parameter : None
+	Output Values   : None
+	Description     : Class definition
+	Approach        : Class defines data member and member function of the queue class
+	*/
 	T data;
 	node* next;
 	friend class linkedlist<T>;
 };
 template <class T>
 class linkedlist{
+	/*
+	Objective       : Creating a class Linkedlist 
+	Input parameter : None
+	Output Values   : None
+	Description     : Class definition
+	Approach        : Class defines data member and member function of the queue class
+	*/
 	node<T>* head;
 	public:
 		linkedlist();
@@ -25,16 +44,37 @@ class linkedlist{
 };
 template <class T>
 linkedlist<T>::linkedlist(){
+	/*
+	Objective       : Creating a constructor of class Linkedlist
+	Input parameter : None
+	Output Values   : None
+	Description     : Constructor definition
+	Approach        : Initialize the elements of class
+	*/
 	head = NULL;
 }
 
 template <class T>
 linkedlist<T>::~linkedlist(){
+	/*
+	Objective       : Creating a destructor of class Linkedlist
+	Input parameter : None
+	Output Values   : None
+	Description     : Destructor definition
+	Approach        : Deleting head
+	*/
 	delete head;
 }
 
 template <class T>
 void linkedlist<T>::insertion(T element){
+	/*
+	Objective       : Inserting in Linked list at the back
+	Input parameter : Element to be inserted
+	Output Values   : None
+	Description     : Member function definition
+	Approach        : Traverse the list till last element and insert the element at the back of linked list
+	*/
 	node<T>* newnode = new node<T>();
 	newnode->next = NULL;
 	newnode->data = element;
@@ -53,6 +93,13 @@ void linkedlist<T>::insertion(T element){
 
 template <class T>
 void linkedlist<T>::display(){
+	/*
+	Objective       : Displaying the elements of Linked list
+	Input parameter : None
+	Output Values   : Elements of linked list
+	Description     : Member function definition
+	Approach        : Traverse the list using while loop and print the elements as well
+	*/
 	node<T>* temp = head;
 	if (temp == NULL){
 		cout << "\nList is empty...!";
@@ -67,6 +114,13 @@ void linkedlist<T>::display(){
 
 template <class T>
 void linkedlist<T>::end_deletion(){
+	/*
+	Objective       : Delete element of Linked list from end
+	Input parameter : None
+	Output Values   : None
+	Description     : Member function definition
+	Approach        : Traverse the list using while loop until last second element and delete last  element
+	*/
 	node<T>* temp = head;
 	if(temp==NULL){
 		cout << "\nLinked list is empty..!";
@@ -88,6 +142,13 @@ void linkedlist<T>::end_deletion(){
 
 template <class T>
 void linkedlist<T>::front_deletion(){
+	/*
+	Objective       : Delete element of Linked list from front
+	Input parameter : None
+	Output Values   : None
+	Description     : Member function definition
+	Approach        : Delete first element from list and adjust head pointer accordingly
+	*/
 	node<T>* temp = head;
 	if(temp==NULL){
 		cout << "\nLinked list is empty..!";
@@ -100,6 +161,14 @@ void linkedlist<T>::front_deletion(){
 
 template <class T>
 void linkedlist<T>::element_deletion(T element){
+	/*
+	Objective       : Delete given element from Linked list 
+	Input parameter : Element that need to be deleted
+	Output Values   : None
+	Description     : Member function definition
+	Approach        : Traverse the list using while loop until to find given element, 
+	                  delete that element and adjust the pointer accordingly
+	*/
 	node<T>* temp = head;
 	if(temp==NULL){
 		cout << "\nLinked list is empty..!";
@@ -125,6 +194,13 @@ void linkedlist<T>::element_deletion(T element){
 
 template <class T>
 void linkedlist<T>::front_insertion(T element){
+	/*
+	Objective       : Inserting in Linked list at front
+	Input parameter : Element to be inserted
+	Output Values   : None
+	Description     : Member function definition
+	Approach        : Insert element at front and adjust head pointer of linked list
+	*/
 	node<T>* newnode = new node<T>();
 	newnode->next = NULL;
 	newnode->data = element;
@@ -140,6 +216,13 @@ void linkedlist<T>::front_insertion(T element){
 }
 
 int main(){
+	/*
+	Objective       : Main function of program
+	Input parameter : None
+	Output Values   : None
+	Description     : Calling the required function of class
+	Approach        : Take input form user and according to the choice the class function is called 
+	*/
 	linkedlist<int> object;
 	int choice;
 	int element;
