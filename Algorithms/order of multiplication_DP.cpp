b072c0114bd69b5cd3877fd21c5e4d2c37d6ae66
Smrity chaudhary <smrity.mca17.du@gmail.com>
2018-10-04 23:31:59 +0530

    second commit

--- Algorithms/order of multiplication_DP.cpp
+++ Algorithms/order of multiplication_DP.cpp
@@ -9,6 +9,13 @@ using namespace std;
 
 int matrixmultiplication(vector<int>);
 int main(){
+  /*
+  Objective       : To take input from user and call matrixmultiplication function
+  Input Parameter : None
+  Output value    : None
+  Description     : Main function of the Programming
+  Approach        : take dimensions of matrices in a vector data structure
+  */
   char choice = 'y';
   vector<int> dimensions;
   while(choice == 'y'){
@@ -22,27 +29,45 @@ int main(){
   matrixmultiplication(dimensions);
 }
 int matrixmultiplication(vector<int> dim){
+  /*
+  Objective       :  To find the minimum cost of matrix multiplication and order
+                     of paranthesis
+  Input Parameter : Dimensions of matrices in vector
+  Output Value    : Cost of matrix multiplication
+  Description     : Calculate the minimum cost of matrix chain multiplication
+  Approach        : Cost will be calculated diagonally (upper diagonal) using
+                    two for loop(d and i) and internally a for loop (K) to track
+                    the correct position of paranthesis.
+  */
   int size = dim.size()-1;
-  int cost[size][size] = { {0,0} };
+  int cost[size][size];
 
-  cout << "cost are: ";
   for (int i=0;i<size;i++){
     for (int j=0;j<size;j++){
-      cout << cost[i][j] << " ";
+      if (i==j){
+        cost[i][j]=0;
+      }
+    else{
+    cost[i][j] = 999;
+      }
     }
-    }
-  for (int i=0;i<size;i++){
-    for (int j=0;j<size;j++){
-      if(i<j){
+  }
+  for (int d=0;d<size;d++){
+    for (int i=0;i<size;i++){
+      int j = i + d;
         for (int k=i;k<j;k++){
           int temp = cost[i][k] + cost[k+1][j] + (dim[i]*dim[k+1]*dim[j+1]);
           if(temp < cost[i][j]){
             cost[i][j] = temp;
           }
         }
-	  }
       }
     }
+  cout << "cost are: \n";
+  for (int i=0;i<size;i++){
+  for (int j=0;j<size;j++){
+    cout << cost[i][j] << " ";
+  }
+  cout<<"\n";
+ }
 }
-  
-
