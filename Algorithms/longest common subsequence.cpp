3c8a3833f53dcf29e0545de422c59ee7f0ee25a6
Smrity chaudhary <smrity.mca17.du@gmail.com>
2018-10-10 23:03:43 +0530

    need review

--- Algorithms/longest common subsequence.cpp
+++ Algorithms/longest common subsequence.cpp
@@ -10,6 +10,12 @@ using namespace std;
 
 void longest_subsequence(vector<char>,vector<char>);
 int main(){
+    /*
+    Objective       : Take input in vector and call other functions
+    Input Parameter : None
+    Output Value    : Sorted Array
+    Description     : Driver function
+    */
     vector<char> seq1;
     vector<char> seq2;
     /*
@@ -49,10 +55,21 @@ int main(){
 
 }
 void longest_subsequence(vector<char> seq1,vector<char> seq2){
+    /*
+    Objective       : To find longest subsequence
+    Input Parameter : Two user entered strings
+    Output Value    : Longest subsequences
+    Approach        : First find length of longest subsequence using Dynamic
+                        programming. Then find all possible longest subsequence
+                        of that length.
+    */
+
     int size_seq1 = seq1.size();
     int size_seq2 = seq2.size();
     vector<char> subseq;
+    //matrix to store matchings in between strings
     int length[size_seq1+1][size_seq2+1];
+    //prepare a matrix for memoization
     for(int i=0;i<=size_seq1;i++){
         for(int j=0;j<=size_seq2;j++){
         if(i==0 || j==0){
@@ -66,6 +83,7 @@ void longest_subsequence(vector<char> seq1,vector<char> seq2){
         }
         }
     }
+    //print the matrix
     cout << "\nmatrix is: \n";
     for(int i=0;i<=size_seq1;i++){
         for(int j=0;j<=size_seq2;j++){
@@ -80,22 +98,27 @@ void longest_subsequence(vector<char> seq1,vector<char> seq2){
     stack<int> sequence;
     sequence.push(size_seq1);
     sequence.push(size_seq2);
+    //Here we will  find out all possible longest sub sequence
+    //This loop will help to backtrack again where the conflict of same length occur
     while(!sequence.empty()){
         j = sequence.top();
         sequence.pop();
         i = sequence.top();
         sequence.pop();
+    //This loop will back track from given point
         while(i>0&&j>0){
         if(seq1[i-1]==seq2[j-1]){
+            //Storing where letters are matching(subsequence)
             lcs.push_back(seq1[i-1]);
             index--;
             i--;
             j--;
         }
         else if(length[i-1][j]==length[i][j-1]){
+            //Push positions into stack where conflict occur
             sequence.push(i-1);
             sequence.push(j);
-            sequence.push(lcs.size());
+            sequence.push(lcs.size()); // to track correct subsequence
             j--;
         }
         else if(length[i-1][j]>length[i][j-1]){
@@ -113,6 +136,7 @@ void longest_subsequence(vector<char> seq1,vector<char> seq2){
     int counter = sequence.top();
     sequence.pop();
     int curr_size = lcs.size();
+    //pop out the letters until where the conflict occurred
     for(int i=1;i<=curr_size-counter;i++){
         lcs.pop_back();
     }
