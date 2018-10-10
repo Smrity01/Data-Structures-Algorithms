e74fc7891ce9f777021c1066e41a2b9d5fd7dce5
Smrity chaudhary <smrity.mca17.du@gmail.com>
2018-10-10 22:41:56 +0530

    almost done

--- Algorithms/longest common subsequence.cpp
+++ Algorithms/longest common subsequence.cpp
@@ -5,6 +5,7 @@ Dated      : 09/10/2018
 */
 #include<iostream>
 #include<vector>
+#include<stack>
 using namespace std;
 
 void longest_subsequence(vector<char>,vector<char>);
@@ -32,17 +33,18 @@ int main(){
     seq1.push_back('a');
     seq1.push_back('b');
     seq1.push_back('c');
-    seq1.push_back('b');
     seq1.push_back('d');
     seq1.push_back('a');
-    seq1.push_back('b');
+    seq1.push_back('f');
+    //seq1.push_back('b');
 
-    seq2.push_back('b');
-    seq2.push_back('d');
+    seq2.push_back('a');
     seq2.push_back('c');
-    seq2.push_back('a');
     seq2.push_back('b');
-    seq2.push_back('a');
+    seq2.push_back('c');
+    seq2.push_back('f');
+    //seq2.push_back('y');
+    //seq2.push_back('b');
     longest_subsequence(seq1,seq2);
 
 }
@@ -72,15 +74,28 @@ void longest_subsequence(vector<char> seq1,vector<char> seq2){
         cout << "\n";
     }
     cout << " longest common subsequence is: \n";
-    int i = size_seq1;
-    int j = size_seq2;
+    int i,j;
     int index = size_seq1-1;
     vector<char> lcs;
-    while(i>0&&j>0){
+    stack<int> sequence;
+    sequence.push(size_seq1);
+    sequence.push(size_seq2);
+    while(!sequence.empty()){
+        j = sequence.top();
+        sequence.pop();
+        i = sequence.top();
+        sequence.pop();
+        while(i>0&&j>0){
         if(seq1[i-1]==seq2[j-1]){
-            lcs[index]= seq1[i-1];
+            lcs.push_back(seq1[i-1]);
             index--;
             i--;
+            j--;
+        }
+        else if(length[i-1][j]==length[i][j-1]){
+            sequence.push(i-1);
+            sequence.push(j);
+            sequence.push(lcs.size());
             j--;
         }
         else if(length[i-1][j]>length[i][j-1]){
@@ -88,9 +103,19 @@ void longest_subsequence(vector<char> seq1,vector<char> seq2){
         }
         else{
             j--;
+
         }
     }
-    for(int i=0;i<lcs.size();i++){
+    for(int i=lcs.size()-1;i>=0;i--){
         cout << lcs[i] << ",";
     }
+    cout << "\n";
+    int counter = sequence.top();
+    sequence.pop();
+    int curr_size = lcs.size();
+    for(int i=1;i<=curr_size-counter;i++){
+        lcs.pop_back();
+    }
+    }
+
 }
