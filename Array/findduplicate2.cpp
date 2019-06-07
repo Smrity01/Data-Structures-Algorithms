edbb5ec5af565ee8a6d064420453ff1cfedc9743
Smrity chaudhary <smrity.mca17.du@gmail.com>
2019-06-07 22:55:49 +0530

    final solution

--- Array/findduplicate2.cpp
+++ Array/findduplicate2.cpp
@@ -2,6 +2,7 @@
 #include<vector>
 #include<stdlib.h>
 #include<math.h>
+#include <unordered_map>
 using namespace std;
 int repeatedNumber(vector<int> A);
 int main(){
@@ -16,9 +17,29 @@ int main(){
     cout << repeatedNumber(A);
 }
 int repeatedNumber(vector<int> A) {
-    int ranges = sqrt()
-    for(int i = 0; i < A.size(); i++){
-        
+    int blockSize = sqrt(A.size());
+    int ranges = (A.size()/blockSize) + 1;
+    int countArray[ranges] = {0};
+    int counter = 0;
+    int index = 0;
+    for (int i = 0; i < A.size(); i++){
+        countArray[(A[i]-1)/blockSize]++;
+    }
+    int selectedBlock = ranges - 1;
+    for (int i = 0; i < ranges - 1; i++){
+        if (countArray > blockSize){
+            selectedBlock = i;
+            break;
+        }
+    }
+    unordered_map<int, int> elementCounts;
+    for (int i = 0; i < A.size(); i++){
+        if( (selectedBlock * blockSize) < A[i] && A[i] <= ((selectedBlock + 1) * blockSize)){
+            elementCounts[A[i]]++;
+            if (elementCounts[A[i]] > 1 ){
+                return A[i];
+            }
+        }
     }
     return -1;
 }
