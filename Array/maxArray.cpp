ea954a3a897dee3f2bac996b4e20a38bd5a037d9
Smrity chaudhary <smrity.mca17.du@gmail.com>
2019-06-11 00:41:23 +0530

    minor changes

--- Array/maxArray.cpp
+++ Array/maxArray.cpp
@@ -1,5 +1,5 @@
 /*
-Given an array of N integers, A1, A2 ,…, AN. Return maximum value of f(i, j) for all 1 ≤ i, j ≤ N.
+Given an array of N integers, A1, A2, …, AN. Return maximum value of f(i, j) for all 1 ≤ i, j ≤ N.
 f(i, j) is defined as |A[i] - A[j]| + |i - j|, where |x| denotes absolute value of x.
 TIME COMPLEXITY : O(n)
 */
